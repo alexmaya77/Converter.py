ad7bd18935558ce34c3d9081807d30f2cafeb83a
alexmaya77 <162768487+alexmaya77@users.noreply.github.com>
2024-11-13 13:10:34 +0000

    Temperature converter

--- templates/converter.py
+++ templates/converter.py
@@ -1,7 +1,7 @@
 temperature_fahrenheit = 100
-print(f"temperature in fahrenheit: {100} °F) 
-          temperature_celsius = (temperature_fahrenheit - 32) * (5 / 9)
-      print( f"temperature in Celsius: {temperature_celsius:.2f} °C")
-      user_input = float(input("100: "))
-      temperature_celsius_input = (user_input - 32) * (5 / 9)
-      print(f"Temperature in Celsius from user input: {temperature_celsius_input:.2f} °C");
+print ( f"temperature in fahrenheit: {100} °F")
+temperature_celsius = (temperature_fahrenheit - 32) * (5 / 9)
+print( f"temperature in Celsius: {temperature_celsius:.2f} °C")
+user_input = float(input("100: "))
+temperature_celsius_input = (user_input - 32) * (5 / 9)
+print(f"Temperature in Celsius from user input: {temperature_celsius_input:.2f} °C")
